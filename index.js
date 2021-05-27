3879d6cc60bdc76cccd5a7baa60b5fb7208ef167
kaustubh natuskar <64963969+Kaustubh-Natuskar@users.noreply.github.com>
2021-05-27 17:56:41 +0530

    updated index file

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.json({ extended: false }));
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000;
+const port = 5000;
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(process.env.PORT || port, () =>
+  console.log(`listening on port ${port}`)
+);
