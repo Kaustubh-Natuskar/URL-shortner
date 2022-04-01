d855d961b8ec3c090034c60c531019a3a4ca64bc
kaustubh <kaustubhnatuskar143@gmail.com>
2022-04-01 19:42:56 +0530

    daar ka mahol hain

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 const express = require('express');
-
-const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+var MemoryStore = require('memorystore')(expressSession)
+const passport = require('passport');
+const flash = require('connect-flash');
 
 const app = express();
 
-connectDB();
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views',);
 
-app.use(express.json({ extended: false }));
+app.use('/static', express.static(__dirname + '/static'));
 
-// Define routes for
-app.use('/', require('./routes/index'));
-app.use('/api/url', require('./routes/url'));
+app.use(express.urlencoded({ extended: true }));
 
-const port = 5000;
+const mongoURI = require('./config/monkoKEY');
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, },).then(() => console.log("Connected !"),);
 
-app.listen(process.env.PORT || port, () =>
-  console.log(`listening on port ${port}`)
-);
+app.use(cookieParser('random'));
+
+app.use(expressSession({
+    secret: "random",
+    resave: true,
+    saveUninitialized: true,
+    // setting the max age to longer duration
+    maxAge: 24 * 60 * 60 * 1000,
+    store: new MemoryStore(),
+}));
+
+app.use(csrf());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use(function (req, res, next) {
+    res.locals.success_messages = req.flash('success_messages');
+    res.locals.error_messages = req.flash('error_messages');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use(require('./controller/routes.js'));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log("Server Started At " + PORT));
